e008c885bd2738ae1c7843558b28bd58c24bf1cf
corzin <jfelipestc@gmail.com>
2026-01-13 10:53:29 +0100

    NATS 13/01/2036 11:00

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,22 +2,24 @@ import { Module } from '@nestjs/common';
 
 import { ProductsController } from './products.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs, PRODUCT_SERVICE } from 'src/config';
+import { envs, NATS_SERVICE, PRODUCT_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
+  imports: [NatsModule],
   controllers: [ProductsController],
   providers: [],
-  imports: [
-    ClientsModule.register([
-      { 
-        name: PRODUCT_SERVICE, 
-        transport: Transport.TCP,
-        options: {
-          host: envs.productsMicroserviceHost,
-          port: envs.productsMicroservicePort
-        }
-      },
-    ]),
-  ],
+  
+  // imports: [
+  //   ClientsModule.register([
+  //     { 
+  //       name: NATS_SERVICE, 
+  //       transport: Transport.NATS,
+  //       options: {
+  //         servers: envs.natsServers
+  //       }
+  //     },
+  //   ]),
+  // ],
 })
 export class ProductsModule {}
